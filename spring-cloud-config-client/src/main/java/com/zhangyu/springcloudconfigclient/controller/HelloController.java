d828d689c0ab394a4924a35ffe3ca162686914f0
zhangyu <zhangyu9069@shein.com>
2019-06-26 15:05:06 +0800

    部分配置内容修改

--- spring-cloud-config-client/src/main/java/com/zhangyu/springcloudconfigclient/controller/HelloController.java
+++ spring-cloud-config-client/src/main/java/com/zhangyu/springcloudconfigclient/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.zhangyu.springcloudconfigclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2019/6/26
  */
 @RestController
+@RefreshScope
 public class HelloController {
     @Value("${hello}")
     String hello;
